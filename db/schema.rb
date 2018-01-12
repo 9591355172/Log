9970d983de3bfd312631fb69c1f482f815001044
Abhijeet <abhijeet428001@gmail.com>
2018-01-12 19:30:26 +0530

    roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108114459) do
+ActiveRecord::Schema.define(version: 20180112135930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 20180108114459) do
     t.text "user_id"
     t.text "what_to_do"
     t.text "what_done"
+    t.boolean "checkbox"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "date"
-    t.boolean "checkbox", default: false
     t.text "completed"
   end
 
@@ -48,11 +48,11 @@ ActiveRecord::Schema.define(version: 20180108114459) do
     t.datetime "in_time"
     t.datetime "out_time"
     t.boolean "is_admin"
-    t.string "roles"
     t.text "name"
     t.string "dept_name"
     t.text "timings_end"
-    t.text "timings"
+    t.json "timings"
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

172ea130e5805c99a686a01d4c076c2a2d65b5a9
Abhijeet <abhijeet428001@gmail.com>
2018-01-16 11:59:20 +0530

    schedulerfinal

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -6,11 +6,9 @@ class HardWorker
     # Do something
     emails = mails.is_a?(Array) ? mails : [mails]
     emails.each do |email|
-    {
     	SendMailMailer.sample_email(email).deliver!
     	puts "EMAIL"
     	puts email
-	}
+    end
   end
-end
 end

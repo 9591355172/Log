3d2bd17e63388321b101af99fdb447789da2c5eb
Abhijeet <abhijeet428001@gmail.com>
2018-01-16 16:17:46 +0530

    timings_email

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -4,10 +4,10 @@ class HardWorker
 
   def perform(mail)
     # Do something
-   		users = User.all
-   		user = users.where(email: mail)
-   		timing = user.timings
-   		timing_end = user.timings_end
+   		@users = User.all
+   		@user = @users.where(email: mail)
+   		timing = @user.timings
+   		timing_end = @user.timings_end
     	SendMailMailer.sample_email(mail).deliver_at(timing)
     	SendMailMailer.sample_email(mail).deliver_at(timing_end)
     	puts "EMAIL"
